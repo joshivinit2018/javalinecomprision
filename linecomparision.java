d37f6a123cf679ba3a708b44dcd10a16f90650fd
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-01 15:27:38 +0530

    [vinit]:add find equalty of line usng comapre to method

--- linecomparision.java
+++ linecomparision.java
@@ -1,46 +1,53 @@
 import java.util.Scanner;
-import java.lang.*;
+class LineComparisonProb{
+        public static void main(String[] args){
+		float x1,y1,x2,y2;
+		double length;
 
-public class linecomparision {
-	// UC-2
+                float x3,y3,x4,y4;
+                double length1,length2;
+                System.out.println("Welcome to Line Comparison Problems.");
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+                Scanner sc = new Scanner(System.in);
+                System.out.println("\n Enter x1 co-ordinate of the line.");
+                x1 = sc.nextFloat();
+      System.out.println("\n Enter y1 co-ordinate of the line.");
+      y1 = sc.nextFloat();
 
-		//Taking input for line 1
-		System.out.print("Enter the point x1: ");
-		int x1 = scan.nextInt();
-		System.out.print("Enter the point y1: ");
-		int y1 = scan.nextInt();
-		System.out.println("(x1,y1) points of the line 1 are: " + x1 + ", " + y1);
+                System.out.println("\n Enter x2 co-ordinate of the line.");
+      x2 = sc.nextFloat();
+      System.out.println("\n Enter y2 co-ordinate of the line.");
+      y2 = sc.nextFloat();
+      System.out.println("\n Coordinates of the line are x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
 
-		System.out.print("Enter the point x2: ");
-		int x2 = scan.nextInt();
-		System.out.print("Enter the point y2: ");
-		int y2 = scan.nextInt();
-		System.out.println("(x2,y2) point of the line 1 are: " + x2 + ", " + y2);
+                length = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+                System.out.println("\nLength of the line : "+ length);
+		System.out.println("\n Enter x3 co-ordinate of the line.");
+      x3 = sc.nextFloat();
+      System.out.println("\n Enter y3 co-ordinate of the line.");
+      y3 = sc.nextFloat();
 
-                //Taking input for line 2
-                System.out.print("Enter the point x3: ");
-                int x3 = scan.nextInt();
-                System.out.print("Enter the point y3: ");
-                int y3 = scan.nextInt();
-                System.out.println("(x3,y3) points of the line 2 are: " + x3 + ", " + y3);
+		System.out.println("\n Enter x4 co-ordinate of the line.");
+      x4 = sc.nextFloat();
+      System.out.println("\n Enter y4 co-ordinate of the line.");
+      y4 = sc.nextFloat();
 
-                System.out.print("Enter the point x4: ");
-                int x4 = scan.nextInt();
-                System.out.print("Enter the point y4: ");
-                int y4 = scan.nextInt();
-                System.out.println("(x4,y4) point of the line 2 are: " + x4 + ", " + y4);
+		System.out.println("\n Coordinates of the line1 is x1="+x1+ " y1="+y1+" and x2="+x2 + " y2="+y2);
+ 		System.out.println("\n Coordinates of the line2 is x3="+x3+ " y3="+y3+" and x4="+x4 + " y4="+y4);
 
-		//Computation for line 1
-		Float length1 = (float) Math.sqrt( (Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2)) );
-		System.out.println("Length of the line 1 is: " + length1);
 
-                //Computation for line 2
-                Float length2 = (float) Math.sqrt( (Math.pow((x4 - x3),2) + Math.pow((y4 - y3),2)) );
-                System.out.println("Length of the line 2 is: " + length2);
+		length1 = Math.sqrt(Math.pow((x2 - x1),2) +  Math.pow((y2 - y1),2));
+		length2 = Math.sqrt(Math.pow((x4 - x3),2) +  Math.pow((y4 - y3),2));	
 
-		System.out.println(length1.equals(length2));
-	}
+      if(length1==length2)
+			System.out.println("\n The lines are Equal. ");
+		else if (length1>length2)
+		System.out.println("\n line1 is gretaer than line2. ");
+		else
+		
+         System.out.println("\n The lines are not Equal. ");        
+	 System.out.println("\n Line2 is gretaer than Line1. ");	
+
 }
+}
+
